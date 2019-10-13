5556d42d3bfc7c77ed3475ae0349f87cf04346c9
samer <s.allaham@ltuc.com>
2019-10-13 17:07:18 +0300

    making text more visible

---
+++ pic.js
@@ -0,0 +1,12 @@
+
+
+
+/*var pic=function () {
+
+    var choice = prompt('How many games do you play regularly?');
+    var picture;
+    }   
+for (let choice = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}*/
