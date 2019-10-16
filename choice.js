34fffa03734d83787a1dcb380318502840af8576
samer <s.allaham@ltuc.com>
2019-10-16 23:06:59 +0300

    updates in thhe site

--- choice.js
+++ choice.js
@@ -2,22 +2,30 @@
 
 
 
+
+
 var game=function () {
+  
     
   var choice = prompt('what game do You like more Dota2 or LOL?');
-  var picture; 
-  while (choice !== 'dota 2' && choice !== 'lol' ){
-   choice = prompt( 'dota 2 or lol?' )
-   }
+ 
   
-  if (choice == 'dota 2') {
+  var background=''; 
+  
+  while (choice !== 'dota 2' && choice !== 'lol' ){
+    choice = prompt( 'dota 2 or lol?' )
+  }
+  // var man = prompt('how many?')
+  // for (var i=0;i<man;i=i+1){
+  
+  if (choice === 'dota 2') {
     document.body.style.background = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOjJGpgeoO2WuinRo-PCWlRmUrm8P1C8hHdJwqP6WXG0Ap_gpb')";
-  } else if (choice == 'lol' ) {
+  } else if (choice === 'lol' ) {
     document.body.style.background = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRruMIbK1GKXa40gnEtWqRAdLZsGmfjlR8deFfbdpuljLzx2WDsEg')";
   }
-  return picture;
+  return background;
    }
   
-
-document.write(game())
+  // }
+document.write(game());
 
--- pic.js
+++ pic.js
@@ -1,12 +1,23 @@
+'use strict'
 
-
-
-/*var pic=function () {
-
-    var choice = prompt('How many games do you play regularly?');
-    var picture;
-    }   
-for (let choice = 0; choice < array.length; choice++) {
-    const element = array[choice];
-    
-}*/
+var option = function(){
+    var order = prompt('which game would you like to try?');
+    //  var total;
+    var picture='';
+    var total;
+    total=prompt( 'how many online games do you play regularly?');
+        // while (total!==text&& total!==text){
+        //     total = prompt('please enter a number')
+        // }
+    for (var ctr = 0; ctr < total; ctr = ctr + 1 ) {
+     if (order === 'lol'){
+         picture=  picture+ '<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWgvMHf8fCTQjrqkfhQ2Xly5q1mhNv7lX9VClILA2DXYQOF3Z5"></p>'
+         }else if (order ==='dota 2') {
+         picture= picture +'<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzXjcICVwcmBkg-3LJCZJQB5-EtAf-Il3-Q1j0INOJ2JCR3Cn2jw"></p>'
+         }
+     
+        }
+        return picture;    
+   }
+document.write(option());
+ 
